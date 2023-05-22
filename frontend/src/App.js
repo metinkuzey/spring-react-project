5a4190a92e9327ce5e808a1f0ed30d3fc1b3c933
metinkuzey <metin.kuzey@outlook.com>
2023-05-22 09:46:32 +0300

    DT-31 #comment First Spring- React Connection Exmp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,33 @@
+import React, { Component } from 'react';
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+class App  extends Component {
+  state = {
+    customers: []
+  };
 
-function App() {
-  return (
-    <div className="bg-stone-100 min-h-screen">
-      <Navbar />
-      <Home />
-    </div>
-  );
+
+  async componentDidMount() {
+    const response = await fetch('/customers');
+    const body = await response.json();
+    this.setState({customers: body});
+  }
+
+  render() {
+    const {customers} = this.state;
+    return (
+      <div className="bg-stone-100 min-h-screen">
+              <Navbar />
+              <Home />
+              <h2>Customers</h2>
+              {customers.map(customer =>
+                  <div key={customer.customerId}>
+                    {customer.userName} ({customer.email})
+                  </div>
+              )}
+            </div>
+    );
+  }
 }
-
 export default App;
+
