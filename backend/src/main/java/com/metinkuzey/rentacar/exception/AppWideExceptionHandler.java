14ffffa6149b9ad3136d85a60c365cc0fa6b0b94
metinkuzey <metin.kuzey@outlook.com>
2023-06-12 23:47:58 +0300

    DT-33 #comment Base Exception Classes, Base Auth Structure with JWT

---
+++ backend/src/main/java/com/metinkuzey/rentacar/exception/AppWideExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.metinkuzey.rentacar.exception;
+
+import com.metinkuzey.rentacar.util.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleException(Exception e) {
+        return new ResponseEntity(new StandardResponse("500", "Error", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    public ResponseEntity handleNotFoundException(RecordNotFoundException e) {
+        return new ResponseEntity(new StandardResponse("404", "Error", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidateException.class)
+    public ResponseEntity handleValidationException(ValidateException e) {
+        return new ResponseEntity(new StandardResponse("400", "Error", e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
+
